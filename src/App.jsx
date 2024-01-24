38945234c8197e2bca70aa574dee4df21c524fa2
Zukizuk <sannimarzuk@gmail.com>
2024-01-24 00:01:51 +0000

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [shownum, setShownum] = useState(false);
 
   function Counter(value) {
     if (count + value < 0) return setCount(0);
@@ -14,8 +15,8 @@ function App() {
   }
   return (
     <>
-      <Header count={count} />
-      <Main count={count} Counter={Counter} />
+      <Header count={count} shownum={shownum} />
+      <Main count={count} setShownum={setShownum} Counter={Counter} />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "../images/logo.svg";
 import Avatar from "../images/image-avatar.png";
 import { useState } from "react";
 
-function Header() {
+function Header({ count, shownum }) {
   const [active, setActive] = useState(false);
   const [cart, setCart] = useState(false);
 
@@ -43,18 +43,23 @@ function Header() {
           </nav>
         </div>
         <div className="right">
-          <svg
-            onClick={() => setCart(!cart)}
-            width="22"
-            height="20"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            className={shownum && count > 0 ? "cart-box show" : "cart-box"}
+            data-cart-items={count}
           >
-            <path
-              d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
-              fill={cart ? "#000" : "#69707D"}
-              fillRule="nonzero"
-            />
-          </svg>
+            <svg
+              onClick={() => setCart(!cart)}
+              width="22"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                fill={cart ? "#000" : "#69707D"}
+                fillRule="nonzero"
+              />
+            </svg>
+          </div>
           <img src={Avatar} alt="Avatar" />
         </div>
         <div className={cart ? "cart show" : "cart"}>
